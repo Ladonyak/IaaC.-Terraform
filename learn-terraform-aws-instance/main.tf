4d9cc3fb0ee47029efd81276774a133441a5a005
Ladonyak <ladonyak@gmail.com>
2022-07-25 14:31:56 +0300

    edit
    ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20220610

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami                    = "ami-052efd3df9dad4825"
+  ami                    = "ami-0729e439b6769d6ab" # ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20220610
   instance_type          = "t2.micro"
 #  vpc_security_group_ids = ["vpc-33591648"]
 #  subnet_id              = "subnet-68a94946"
